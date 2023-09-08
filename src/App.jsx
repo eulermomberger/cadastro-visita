1121c1c9de3ba7babba901e6dc0eb5c034923b3e
Euler <mombergereuler@gmail.com>
2023-09-07 22:44:57 -0300

    feat: criando o painel de visitantes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react';
+import { collection, getDocs, Timestamp } from 'firebase/firestore/lite';
+
+import { firestore } from './firebase';
+
 import { Header } from './components/Header';
+import { VisitorsList } from './components/VisitorsList';
 
 function App() {
+  const [visitors, setVisitors] = useState([]);
+
+  const fetchVisitors = async () => {
+    const visitorsCollection = collection(firestore, 'visitors');
+    const visitorSnapshot = await getDocs(visitorsCollection);
+    const visitorList = visitorSnapshot.docs.map((doc) => ({ ...doc.data(), uuid: doc.id }));
+    setVisitors(visitorList);
+  };
+
+  useEffect(() => {
+    fetchVisitors();
+  }, []);
+
   return (
     <div>
       <Header />
+
+      <VisitorsList visitors={visitors}/>
     </div>
   );
 }
---
+++ src/components/VisitorsList/index.jsx
@@ -0,0 +1,37 @@
+import './styles.css';
+
+import { MdAccountCircle } from 'react-icons/md';
+
+export function VisitorsList({ visitors }) {
+  return (
+    <div className='list-container'>
+      <div className='list-header'>
+        <span className='list-icon'></span>
+        <span className='list-name'>Nome</span>
+        <span className='list-rg'>RG</span>
+        <span className='list-date'>Data de modificação</span>
+      </div>
+      <div className='list-body'>
+        {visitors.map((visitor) => (
+          <div key={visitor.uuid} className='list-visitor-row'>
+            <div className='list-icon'>
+              <MdAccountCircle
+                color='#FFF'
+                size={36}
+              />
+            </div>
+            <div className='list-name'>
+              {visitor.name}
+            </div>
+            <div className='list-rg'>
+              {visitor.rg}
+            </div>
+            <div className='list-date'>
+              {visitor.updated_at.toDate().toLocaleDateString()}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
